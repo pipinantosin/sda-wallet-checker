45a4c4197a578124e07df3fa1b4cf275ff7290b5
Pipin Antosin <77954264+pipinantosin@users.noreply.github.com>
2026-04-14 23:21:43 +0700

    push zip

---
+++ js/ui-core.js
@@ -0,0 +1,13 @@
+// ==========================
+// UI CORE (GLOBAL ELEMENTS)
+// ==========================
+window.balanceEl = document.getElementById("balance");
+window.selectEl = document.getElementById("walletSelect");
+
+window.addressInput = document.getElementById("address");
+window.saveBtn = document.querySelector("button[onclick='saveWallet()']");
+
+window.tokenLogoBalance = document.getElementById("tokenLogoBalance");
+window.tokenLogoDropdown = document.getElementById("tokenLogoDropdown");
+
+window.selectedToken = "native"; // default SDA
